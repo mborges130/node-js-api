66c5131494480671363a0befe0f1146525578128
Mateus Borges <mateusdantas73@icloud.com>
2025-07-24 00:44:00 -0300

    Adicionando arquivos package.json e server.js iniciais para a API do Node.js

---
+++ server.js
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Curso de Node.JS\n');
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+}
+);
